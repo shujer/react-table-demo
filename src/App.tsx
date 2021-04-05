edbecb231bc86bd74bd4f6a055ec8ddf5b97bac2
shujer <dwellerofuniverse@gmail.com>
2021-04-05 18:10:57 +0800

    feat: add antd resizable column demo

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import BaseTable from "./BaseTable/TableTest";
 import GridTable from "./GridTable/TableTest";
+import ResizeTable from "./ResizableColumn/TableTest";
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <BaseTable />
       <br />
       <GridTable />
+      <br />
+      <ResizeTable />
     </div>
   );
 }
